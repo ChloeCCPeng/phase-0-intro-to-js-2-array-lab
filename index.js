285639c04ea90057138be00d1de6283dcf491c53
Chloe Peng <chloepeng331@icloud.com>
2022-01-10 22:24:17 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+const cats =["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+    cats.splice(-1);
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.splice(0,1);
+}
+
+function appendCat(name){
+    return [...cats, "Broom"];
+}
+
+function prependCat(name){
+    return ["Arnold", ...cats];
+}
+
+function removeLastCat (){
+    return cats.slice(0,-1);
+}
+function removeFirstCat (){
+    return cats.slice(1);
+}
