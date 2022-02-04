386aa42c858c7a4682dcd0aeb827ec37c5faee39
Chloe Peng <chloepeng331@icloud.com>
2022-02-04 11:34:22 -0500

    commit tree b1b5e31e4e486886286b8ed353d414b5262260e5

--- index.js
+++ index.js
@@ -1,33 +1,70 @@
-// Write your solution here!
-const cats =["Milo", "Otis", "Garfield"];
+const cats = ['Milo', 'Otis', 'Garfield'];
 
-function destructivelyAppendCat(name){
-    cats.push("Ralph");
-}
+const destructivelyAppendCat = name => (cats.push("Ralph"));
 
-function destructivelyPrependCat(name){
-    cats.unshift("Bob");
-}
+const destructivelyPrependCat = name => (cats.unshift("Bob"));
 
-function destructivelyRemoveLastCat(){
-    cats.splice(-1);
-}
+const destructivelyRemoveLastCat = name => (cats.pop("Milo", "Otis"));
 
-function destructivelyRemoveFirstCat(){
-    cats.splice(0,1);
-}
+const destructivelyRemoveFirstCat = name => (cats.shift("Otis", "Garfield"));
 
-function appendCat(name){
-    return [...cats, "Broom"];
-}
+const appendCat = name => [...cats, "Broom"];
 
-function prependCat(name){
-    return ["Arnold", ...cats];
-}
+const prependCat = name => ['Arnold', ...cats];
 
-function removeLastCat (){
-    return cats.slice(0,-1);
-}
-function removeFirstCat (){
-    return cats.slice(1);
-}
+const removeLastCat = name => cats.slice(0, cats.length - 1);
+
+const removeFirstCat = name => cats.slice(1, cats.length);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Write your solution here!
+// const cats =["Milo", "Otis", "Garfield"];
+
+// function destructivelyAppendCat(name){
+//     cats.push("Ralph");
+// }
+
+// function destructivelyPrependCat(name){
+//     cats.unshift("Bob");
+// }
+
+// function destructivelyRemoveLastCat(){
+//     cats.splice(-1);
+// }
+
+// function destructivelyRemoveFirstCat(){
+//     cats.splice(0,1);
+// }
+
+// function appendCat(name){
+//     return [...cats, "Broom"];
+// }
+
+// function prependCat(name){
+//     return ["Arnold", ...cats];
+// }
+
+// function removeLastCat (){
+//     return cats.slice(0,-1);
+// }
+// function removeFirstCat (){
+//     return cats.slice(1);
+// }
